7ea78db16fe9ea36ccf99c18de0a72e6d54ad283
mangomadhava <madhavapaliyam@gmail.com>
2021-05-23 14:37:20 -0400

    debugging

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -49,16 +49,26 @@ def remove_folders():
     for var, obj in local_vars:
         print(var, sys.getsizeof(obj))
 
-    start_path = '.'
-    total_size = 0
-    for dirpath, dirnames, filenames in os.walk(start_path):
-        for f in filenames:
-            fp = os.path.join(dirpath, f)
-            # skip if it is symbolic link
-            if not os.path.islink(fp):
-                total_size += os.path.getsize(fp)
+    # start_path = '.'
+    # total_size = 0
+    # for dirpath, dirnames, filenames in os.walk(start_path):
+    #     for f in filenames:
+    #         fp = os.path.join(dirpath, f)
+    #         # skip if it is symbolic link
+    #         if not os.path.islink(fp):
+    #             total_size += os.path.getsize(fp)
     print(os.listdir('.'))
-    print(total_size, 'bytes')
+    for dir in os.listdir('.'):
+        start_path = './' + dir
+        total_size = 0
+        for dirpath, dirnames, filenames in os.walk(start_path):
+            for f in filenames:
+                fp = os.path.join(dirpath, f)
+                # skip if it is symbolic link
+                if not os.path.islink(fp):
+                    total_size += os.path.getsize(fp)
+
+        print('start_path', total_size, 'bytes')
 
 sched = BackgroundScheduler(daemon=True)
 sched.add_job(remove_folders,'interval', minutes = 1, id='remove_temp_folders')
