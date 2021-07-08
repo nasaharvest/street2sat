895e933ff210f35647c1ebb5a5898421e1bea5ad
ivanzvonkov <ivan.zvonkov@gmail.com>
2021-07-07 17:43:01 -0700

    Script for deploy resources

---
+++ deploy.sh
@@ -0,0 +1,20 @@
+export TAG=us-central1-docker.pkg.dev/bsos-geog-harvest1/street2sat/street2sat
+export BUCKET=street2sat-uploaded
+export URL="https://street2sat-grxg7bzh2a-uc.a.run.app"
+
+docker build . -t $TAG
+docker push $TAG
+gcloud run deploy street2sat --image ${TAG}:latest \
+        --memory=4Gi \
+        --platform=managed \
+        --region=us-central1 \
+        --allow-unauthenticated
+
+gcloud functions deploy trigger-street2sat \
+    --source=gcp/trigger_inference_function \
+    --trigger-bucket=$BUCKET \
+    --allow-unauthenticated \
+    --runtime=python39 \
+    --entry-point=hello_gcs \
+    --set-env-vars INFERENCE_HOST="$URL" \
+    --timeout=300s
