898feeea17526f37629798692f1e8b16da56a780
mangomadhava <madhavapaliyam@gmail.com>
2021-09-19 12:51:26 -0400

    nothing again

--- gcp/utils/download_matched_files.py
+++ gcp/utils/download_matched_files.py
@@ -22,6 +22,13 @@ def main(bucket_path, download_path):
         blob_path = blob_path.replace('.jpg', '.JPG')
         blob = download_bucket.blob(blob_path)
         blob.download_to_filename(download_path + blob_path.split('/')[-1])
-        
+
+
+'''
+Finds and downloads files from street2sat-uploaded that are in a folder in the street2sat-model-predictions
+Usage:
+python download_matched_files.py <path to folder with images in google cloud> <local directory>
+ex. python download_matched_files.py street2sat-model-predictions/KENYA/2021-07-05-T1 /Users/madhavapaliyam/Downloads/
+'''
 if __name__ == "__main__":
     main(sys.argv[1], sys.argv[2])
