5e6443d8f190d67bf0a38bf50ee9e5f24f8ec770
mangomadhava <madhavapaliyam@gmail.com>
2022-05-16 23:19:43 -0400

    Re-doing notebooks to make them cleaner in progress

--- scripts/update_csv_from_firebase.py
+++ scripts/update_csv_from_firebase.py
@@ -6,7 +6,7 @@ pd.options.display.max_colwidth = 600
 pd.options.display.max_columns = None
 pd.options.display.max_rows = None
 import collections
-import time
+import reverse_geocoder as rg
 
 '''
 This script updates the csv located inside 'street2sat-database-csv' bucket 
@@ -19,6 +19,10 @@ Currently it does not store predictions, only these attributes:
 'longitude': longitude from the image metadata
 'being_labeled': if the current image is being labeled or not
 'country': country the image was taken in, retrieved from the folder name 
+'admin1': admin1 for the location (retrieved from reverge_geocoder library)
+'admin2': admin2 for the location (retrieved from reverse_geocoder library)
+'cc': Country code (retreived from reverse_geocoder library)
+'location': Town location (retrieved from reverse_geocoder library)
 'test_set': if the current image is part of the test set TODO
 'time': time from the image metadata
 'focal_length': focal length from the image metadata
@@ -38,20 +42,20 @@ only store the latest predictions.
 
 
 
-def get_images_already_being_labelled():
-    """Gets images already labelled"""
-    images_already_being_labelled = []
-    csv_names = [blob.name for blob in client.list_blobs('street2sat-gcloud-labeling', prefix="") if blob.name.endswith(".csv")]
+def get_images_already_being_labeled():
+	"""Gets images already labeled"""
+	images_already_being_labelled = []
+	csv_names = [blob.name for blob in client.list_blobs('street2sat-gcloud-labeling', prefix="") if blob.name.endswith(".csv")]
 
-    for csv_name in tqdm(csv_names, desc="Get already labelled"):
-        uris = pd.read_csv(f"gs://street2sat-gcloud-labeling/{csv_name}", header=None, sep="\n")[0]
-        images_already_being_labelled += uris.to_list()
+	for csv_name in tqdm(csv_names, desc="Get already labeled"):
+		uris = pd.read_csv(f"gs://street2sat-gcloud-labeling/{csv_name}", header=None, sep="\n")[0]
+		images_already_being_labelled += uris.to_list()
 
-    # Ensure there are no duplicates in images already being labelled
-    dupes = [item for item, count in collections.Counter(images_already_being_labelled).items() if count > 1]
-    dupes.remove('0') # An index of 0 was erroneously output in previous csv
-    assert len(dupes) == 0, "Found duplicates in images being labelled. One of the labeling tasks needs to be removed."
-    return set(images_already_being_labelled)
+	# Ensure there are no duplicates in images already being labelled
+	dupes = [item for item, count in collections.Counter(images_already_being_labelled).items() if count > 1]
+	dupes.remove('0') # An index of 0 was erroneously output in previous csv
+	assert len(dupes) == 0, "Found duplicates in images being labeled. One of the labeling tasks needs to be removed."
+	return set(images_already_being_labelled)
 
 
 
@@ -72,7 +76,7 @@ test_set_info = pd.read_csv('gs://street2sat-database-csv/test-set.csv')
 test_set_imgs = set(test_set_info['input_img'])
 
 # to upadate any images from not being labaled to being labeled 
-already_labeled = get_images_already_being_labelled()
+already_labeled = get_images_already_being_labeled()
 
 
 lat = []
@@ -80,6 +84,7 @@ lon = []
 name = []
 being_labeled = []
 country = []
+location = []
 test_set = []
 time_d = []
 focal_length = []
@@ -91,61 +96,72 @@ i = 0
 limit_amount = 10000
 
 test_num = 0
-nones_detected = 0
+nones_indexes = []
+
+
 
 docs = coll.order_by('input_img').limit(limit_amount).stream()
 while True:
-    p_i = i 
-    for image in docs: 
-        d_image = image.to_dict()
+	p_i = i 
+	for image in docs: 
+		d_image = image.to_dict()
+		if d_image['coord'][0] == None or d_image['coord'][1] == None: 
+			nones_indexes.append(i)
+			# temporarily append a  0,0 so that reverge_geocoder works right, fix later 
+			location.append((0,0))
+		else: 
+			location.append(tuple(d_image['coord']))
+		 
+		lat.append(d_image['coord'][0])
+		lon.append(d_image['coord'][1])
+		
 
-        if d_image['coord'][0] == None or d_image['coord'][1] == None: 
-            nones_detected += 1
-            continue
+		name.append(d_image['input_img'])
+		if d_image['input_img'] not in dup_checker:
+			dup_checker.add(d_image['input_img'])
+		else: 
+			print(f"Duplicate detected: {d_image} {i}")
 
-        lat.append(d_image['coord'][0])
-        lon.append(d_image['coord'][1])
-        name.append(d_image['input_img'])
-        if d_image['input_img'] not in dup_checker:
-            dup_checker.add(d_image['input_img'])
-        else: 
-            print(f"Duplicate detected: {d_image} {i}")
+		time_d.append(d_image['time'])
+		focal_length.append(d_image['focal_length'])
+		pixel_height.append(d_image['pixel_height'])
 
-        time_d.append(d_image['time'])
-        focal_length.append(d_image['focal_length'])
-        pixel_height.append(d_image['pixel_height'])
+		# weird extra folder case 
+		if d_image['input_img'].split('/')[3].startswith('2021'):
+			country.append(None)
+		else: 
+			country.append(d_image['input_img'].split('/')[3])
+		
 
-        # weird extra folder case 
-        if d_image['input_img'].split('/')[3].startswith('2021'):
-            country.append('NA')
-        else: 
-            country.append(d_image['input_img'].split('/')[3])
-        
+		if d_image['input_img'] in already_labeled: 
+			being_labeled.append(True)
+		else: 
+			being_labeled.append(False)
 
-        if d_image['input_img'] in already_labeled: 
-            being_labeled.append(True)
-        else: 
-            being_labeled.append(False)
+		if d_image['input_img'] in test_set_imgs: 
+			test_num += 1
+			test_set.append(True)
+		else:
+			test_set.append(False)
 
-        if d_image['input_img'] in test_set_imgs: 
-            test_num += 1
-            test_set.append(True)
-        else:
-            test_set.append(False)
-
-        i += 1
+		i += 1
 
 
-    # multiple queries, next one starts where the previous ended 
-    docs = coll.order_by('input_img').start_after({'input_img':name[-1]}).limit(limit_amount).stream()
-    assert len(lat) == len(lon) == len(name) == len(being_labeled) == len(country) == len(test_set) == len(time_d) == len(focal_length) == len(pixel_height), 'lengths incorrect'
-    # these don't print every 10000 bc some images dont have locations 
-    print(f"Querying street2sat uploaded: {i}, {len(name)}")
-    print(f"Test set images detected: {test_num}")
-    print(f"None for locations detected: {nones_detected}")
-    if p_i == i: 
-        break
-    
+	# multiple queries, next one starts where the previous ended 
+	docs = coll.order_by('input_img').start_after({'input_img':name[-1]}).limit(limit_amount).stream()
+	assert len(lat) == len(lon) == len(name) == len(being_labeled) == len(country) == len(test_set) == len(time_d) == len(focal_length) == len(pixel_height), 'lengths incorrect'
+	# these don't print every 10000 bc some images dont have locations 
+	print(f"Querying street2sat uploaded: {i}, {len(name)}")
+	print(f"Test set images detected: {test_num}")
+	if p_i == i: 
+		break
+	
+
+
+results = rg.search(location)
+assert len(results) == len(lat), "Results and length of other data are off"
+df_location_data = pd.DataFrame(results)
+
 
 df = pd.DataFrame()
 df['input_img'] = name
@@ -153,10 +169,20 @@ df['latitude'] = lat
 df['longitude'] = lon
 df['being_labeled'] = being_labeled
 df['country'] = country
+df['admin1'] = df_location_data['admin1']
+df['admin2'] = df_location_data['admin2']
+df['cc'] = df_location_data['cc']
+df['location'] = df_location_data['name']
 df['test_set'] = test_set 
 df['time'] = time_d 
 df['focal_length'] = focal_length
 df['pixel_height'] = pixel_height
+
+# fix images with no location 
+df.loc[nones_indexes, 'admin1'] = None 
+df.loc[nones_indexes, 'admin2'] = None 
+df.loc[nones_indexes, 'cc'] = None 
+df.loc[nones_indexes, 'location'] = None 
 
 # upload to bucket
 blob = uploaded_bucket.blob('database-info.csv')
